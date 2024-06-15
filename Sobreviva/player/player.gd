490367e88633e47d06e06310024f4d58276d8bc2
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2024-06-15 09:56:23 -0300

    att

---
+++ Sobreviva/player/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+class_name Player
+
+@export_category("Variables")
+@export var _move_speed: float = 256.0
+
+func _physics_process(delta):
+	_move()
+	
+func _move():
+	var _direction: Vector2 = Input.get_vector(
+		"move_left", "move_right",
+		"move_up", "move_down"
+	)
+	
+	velocity = _direction * _move_speed
+	move_and_slide()
