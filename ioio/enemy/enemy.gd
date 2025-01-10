72ece32593249d49b1bc97f361e280e26eced12c
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-01-10 10:45:47 -0300

    ,

--- ioio/enemy/enemy.gd
+++ ioio/enemy/enemy.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 class_name Enemy_base
 
-var direction = Vector2(1,0)
+var direction = Vector2(0, 1)
+
+
+@onready var top_point = $TopPoint.global_position.y
+@onready var botton_point = $BottonPoint.global_position.y
 
 @export_category("Variables")
 @export var _enemy_health: int = 5
@@ -10,10 +14,24 @@ var direction = Vector2(1,0)
 func _ready():
 	get_gravity()
 
+
+
 func _physics_process(delta: float):
 	velocity = direction * speed
 	move_and_slide()
-
+	if is_on_wall():
+		direction = -direction  # Inverte a direção
+	# Verifica se atingiu os limites
+	if global_position.y <= top_point:
+		direction = Vector2(0, 1)  # Muda para baixo
+	elif global_position.y >= botton_point:
+		direction = Vector2(0, -1)  # Muda para cima
+func _process(delta):
+	position += direction * speed * delta
+# Confinar o movimento no mapa (exemplo: 800x600)
+	position.x = clamp(position.x, 0, 454)
+	position.y = clamp(position.y, 0, 175)
+	
 func update_health(_damage: int) -> void:
 	_enemy_health -= _damage
 	if _enemy_health <= 0:
