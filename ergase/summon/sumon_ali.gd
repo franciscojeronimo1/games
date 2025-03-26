face0c994cd021c8c332d7580c61be04a8a79053
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-03-26 20:16:31 -0300

    .

--- ergase/summon/sumon_ali.gd
+++ ergase/summon/sumon_ali.gd
@@ -1,15 +1,12 @@
 extends CharacterBody2D
 
-@onready var enemy: CharacterBody2D = $"."
-
-
 @export var speed: float = 50  # Velocidade de movimento
 @export var gravity: float = 800  # Gravidade
 @export var detection_range: float = 200  # Distância para detectar o jogador
 
 
-
-@onready var player = get_parent().find_child("Player")  # Encontra o jogador na cena
+@onready var aliado = get_parent().find_child('Player') 
+@onready var player = get_parent().find_child("Enemy")  # Encontra o jogador na cena
 @onready var sprite = $AnimatedSprite2D  # Referência ao sprite do inimigo
 
 func _physics_process(delta):
@@ -27,13 +24,10 @@ func follow_player():
 		
 	var direction = (player.position - position).normalized()  # Direção até o player
 	var distance = position.distance_to(player.position)  # Distância ao player
+	var directionAliado = (aliado.position - position).normalized()  # Direção até o player
+	var distanceAliado = position.distance_to(aliado.position)  # Distância ao player
 	if distance <= detection_range:
 		velocity.x = direction.x * speed  # Move na direção do player
 		sprite.flip_h = velocity.x > 0  # Vira o sprite conforme a direção
 	else:
-		velocity.x = 0  # Para quando está longe do player
-
-
-func _on_hp_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		enemy.queue_free()
+		velocity.x = directionAliado.x * speed 
