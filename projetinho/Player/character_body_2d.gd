7bb423a524dfea0f5590766e8249a9bdbd4401eb
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-05-16 10:33:58 -0300

    ,

--- projetinho/Player/character_body_2d.gd
+++ projetinho/Player/character_body_2d.gd
@@ -1,25 +1,32 @@
 extends CharacterBody2D
 
+@export var gravity = 800
+@export var jump_speed = -225
+@export var move_speed = 100
+@onready var animation_player: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+func _physics_process(delta):
+	anim()
+	quitar()
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.y += gravity * delta
+	if is_on_floor() and Input.is_action_just_pressed("ui_jump"):
+		velocity.y = jump_speed
+		animation_player.play("jump") 
 	move_and_slide()
+
+func anim():
+	var input_dir = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.x = input_dir * move_speed
+	if is_on_floor() and velocity.x == 0:
+		animation_player.play("idle")
+	elif is_on_floor() and velocity.x != 0:
+		animation_player.play("run")
+	if velocity.x > 0:
+		animation_player.flip_h = false  # Virado para a direita
+	elif velocity.x < 0:
+		animation_player.flip_h = true
+		
+func quitar():
+	if Input.is_action_just_pressed("esc"):
+		get_tree().quit()
