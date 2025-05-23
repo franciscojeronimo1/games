08d6987d05ef0b96524459c32c787ccbdb121117
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-05-23 11:16:40 -0300

    ,

--- projetinho/enemy/enemy.gd
+++ projetinho/enemy/enemy.gd
@@ -1,25 +1,31 @@
 extends CharacterBody2D
 
+@onready var state_machine = $StateMachine
+@onready var anim = $AnimationPlayer
+@onready var player_detector = $Area2D
+@export var speed := 40
+var player_in_range := false
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _ready():
+	state_machine.init(self)
+	player_detector.body_entered.connect(_on_player_entered)
+	player_detector.body_exited.connect(_on_player_exited)
 
+func _physics_process(delta):
+	state_machine.state_update(delta)
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func play_animation(name: String):
+	anim.play(name)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func change_state(new_state: String):
+	state_machine.change_state(new_state)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func _on_player_entered(body):
+	if body.name == "Player":
+		 player_in_range = true
+		change_state("attack")
 
-	move_and_slide()
+func _on_player_exited(body):
+	if body.name == "Player":
+		player_in_range = false
+		change_state("walk")
--- projetinho/enemy/idle.gd
+++ projetinho/enemy/idle.gd
@@ -1,1 +1,11 @@
-extends Node2D
+extends Node
+
+func enter(enemy):
+	enemy.velocity = Vector2.ZERO
+	enemy.play_animation("idle")
+
+func update(enemy, delta):
+	# Transição automática para walk depois de 2 segundos
+	await get_tree().create_timer(2).timeout
+	if not enemy.player_in_range:
+		enemy.change_state("walk")
